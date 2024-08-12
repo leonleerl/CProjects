9406369110d0ef8402b15f3012a437e5d9a834d2
leonleerl <leon.runlongli@gmail.com>
2024-08-12 12:09:07 +0800

    week4 lecture at classroom

---
+++ week4/Monday/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    for (int i = 0; i < 4; i++)
+    {
+        /* code */
+        register int banana = 1;
+        printf("%d\n", banana);
+    }
+    int a = 3 + 2;
+    return 0;
+}
